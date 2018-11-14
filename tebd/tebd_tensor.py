332f539c1b5cf61e9db371fc4bd42625c2ff6b30
Li SH <li012589li@outlook.com>
2018-11-14 15:49:48 +0800

    update

--- tebd/tebd_tensor.py
+++ tebd/tebd_tensor.py
@@ -6,10 +6,10 @@ import argparse
 parser = argparse.ArgumentParser(description="")
 
 parser.add_argument("-J",type = float,default=1,help="")
-parser.add_argument("-Hx",type = float,default=0,help="")
+parser.add_argument("-Hx",type = float,default=0.5,help="")
 parser.add_argument("-delta",type = float,default=0.001,help="")
-parser.add_argument("-iters",type = int,default=1000,help="")
-parser.add_argument("-cut", type = int, default=5,help="")
+parser.add_argument("-iters",type = int,default=10000,help="")
+parser.add_argument("-cut", type = int, default=10,help="")
 
 
 args = parser.parse_args()
@@ -26,9 +26,9 @@ I2 = torch.eye(2).to(dtype=torch.float64)
 
 H = -args.J*torch.einsum("ij,kl->ikjl",sz,sz)+0.5*-args.Hx*(torch.einsum("ij,kl->ikjl",sx,I2)+torch.einsum("ij,kl->ikjl",I2,sx))
 
-H = H*-args.delta
+Hp = H*-args.delta
 
-e,v = torch.eig(H.reshape(4,4),True)
+e,v = torch.eig(Hp.reshape(4,4),True)
 E = torch.exp(e[:,0])
 U = torch.matmul(torch.matmul(v,torch.diag(E)),v.t()).reshape(2,2,2,2)
 
@@ -38,9 +38,30 @@ for i in range(args.iters):
     u,s,vt = torch.svd(thetap)
     s = s[:args.cut]
     u = u[:,:args.cut].reshape(args.cut,2,args.cut)
-    v = v.t()[:args.cut,:].reshape(args.cut,2,args.cut)
+    v = vt.t()[:args.cut,:].reshape(args.cut,2,args.cut)
     lamA = s/(torch.sqrt((s**2).sum()))
     lamBinv = lamB**-1
     GA = torch.einsum("ab,bcd->acd",torch.diag(lamBinv),u)
     GB = torch.einsum("abc,cd->abd",v,torch.diag(lamBinv))
+
+    (lamA,lamB) = (lamB,lamA)
+    (GA,GB) = (GB,GA)
+
+theta = torch.einsum("ab,bcd,de,efg,gh->acfh",torch.diag(lamB),GA,torch.diag(lamA),GB,torch.diag(lamB))
+thetaU = torch.einsum("abcd,bcef->aefd",theta,H)
+
+E = torch.einsum("abcd,abcd->",theta,thetaU)
+
+(lamA,lamB) = (lamB,lamA)
+(GA,GB) = (GB,GA)
+
+theta = torch.einsum("ab,bcd,de,efg,gh->acfh",torch.diag(lamB),GA,torch.diag(lamA),GB,torch.diag(lamB))
+thetaU = torch.einsum("abcd,bcef->aefd",theta,H)
+
+E += torch.einsum("abcd,abcd->",theta,thetaU)
+
+E /= 2
+
+print("E:",E.item())
+
 
